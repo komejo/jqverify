bc9ffd41a3afddd6c300ac915109e573e434eb1b
Joe Komenda <jkomenda@asheavenue.com>
2013-08-26 15:57:29 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -2,3 +2,23 @@ jqverify
 ========
 
 An age verification (or age gate) using HTML, CSS, and jQuery/JavaScript.
+
+This age verification system is set up for Legal Drinking Age, has a JS cookie option, and works in a modal window. It could easily be adapted for other uses.
+
+Most age verification use a mix of server and client-side code, but since age they rely 100% on honest input, there's no reason to do this.
+
+When I went looking for a complete solution that was frontend-only, I couldn't find anything that had all of the HTML with the countries / drinking ages included, set cookies, or had a 'remember me' ckeckbox, so I cobbled this together.
+
+## Basic Use
+
+
+
+## License and Credits
+
+© 2013 <a href="https://github.com/komejo">Komejo</a>. Created by <a href="http://twitter.com/KomejoDev">Joe Komenda</a>.
+
+Minified using http://refresh-sf.com/yui/
+
+Credit to [Cats Who Code](http://www.catswhocode.com/blog/10-jquery-snippets-for-efficient-developers) for the 'Validate a date of birth using jQuery' section.
+jqverify.js is released under the <a href="http://opensource.org/licenses/MIT">MIT license</a>.
+
---
+++ jqverify.js
@@ -0,0 +1,78 @@
+jQuery(document).ready(function() {
+
+    var agemodal = "#agemodal",
+        submit   = "#formsubmit",
+        content  = "#content";
+
+    if (!readCookie('jqverify')) {
+        jQuery(agemodal).fadeIn();
+    } else {
+        jQuery(content).fadeIn();
+    };
+
+    jQuery(formsubmit).on('click', function(event){
+        event.preventDefault ? event.preventDefault() : event.returnValue = false;
+
+        var day, month, year, age, remember, mydate, currdate;
+
+        jQuery("#day").val() ? day = jQuery("#day").val() : day = 'notset';
+        jQuery("#month").val() ? month = jQuery("#month").val() : month = 'notset';
+        jQuery("#year").val() ? year = jQuery("#year").val() : year = 'notset';
+        age = jQuery("#location").val();
+        jQuery("#remember").is(':checked') ? remember = 'checked' : remember = false;
+
+        mydate = new Date();
+        mydate.setFullYear(year, month-1, day);
+        currdate = new Date();
+        currdate.setFullYear(currdate.getFullYear() - age);
+
+        if (day == 'notset' || month == 'notset' || year == 'notset' ) {
+            alert("Please enter your birthdate.");
+            return false;
+        } else if (age == 99 ) {
+            alert("Sorry, persons from your country are not permitted to view this site");
+            return false;
+        } else if ((currdate - mydate) < 0) {
+            alert("Sorry, only persons over the age of " + age + " may enter this site");
+            return false;
+        } else {
+            if (remember) {
+                createCookie('jqverify', 1, 3650); // expire in 10 years
+            } else {
+                createCookie('jqverify', 1, 1); // expire in a day
+            };
+            jQuery(agemodal).fadeOut();
+            jQuery(content).fadeIn();
+            return true;
+        }
+    });
+
+    function createCookie(name,value,days) {
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime()+(days*24*60*60*1000));
+            var expires = "; expires="+date.toGMTString();
+        }
+        else var expires = "";
+        document.cookie = name+"="+value+expires+"; path=/";
+
+        console.log(name+"="+value+expires+"; path=/")
+    };
+
+    function readCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+    };
+
+    // A handy little function to erase cookies - useful when testing/debugging.
+    function eraseCookie(name) {
+        createCookie(name,"",-1);
+    };
+
+});
